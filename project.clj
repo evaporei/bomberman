dfcd85fa557204595002c773d1344f03d62c9c11
Otávio Pace <otaviopp8@gmail.com>
2018-12-12 08:10:58 -0200

    :bomb: first working version! :tada:

--- project.clj
+++ project.clj
@@ -5,8 +5,11 @@
   :license {:name "WTFPL"
             :url "http://www.wtfpl.net/txt/copying/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.2.0"]
+                 [http-kit "2.3.0-alpha5"];; this version is because of protocolexception, which is related to SNI: https://medium.com/@kumarshantanu/using-server-name-indication-sni-with-http-kit-client-f7d92954e165
                  [cheshire "5.8.1"]
-                 [compojure "1.6.0"]]
-  ;; :profiles {:uberjar {:aot [bomberman.core]}}
+                 [compojure "1.6.0"]
+                 [clj-fuzzy "0.4.1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [ring/ring-json "0.4.0"]
+                 [lynxeyes/dotenv "1.0.2"]]
   :main bomberman.core)
